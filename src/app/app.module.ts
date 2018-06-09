aef3b2108f7667a2222c153172a36fe5eb2b2d13
Joe Parsley <joewparsley@gmail.com>
2018-06-08 23:24:31 -0700

    install angular materiall and animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Browser Animations Required by Angular Material
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Angular Material Component Import
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
