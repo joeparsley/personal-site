8b42245a775f38b9a210b4d26098cfad5a5ab172
Joe Parsley <joewparsley@gmail.com>
2018-06-17 20:51:25 -0700

    add ngfor loop to project section, to loop through cardData in the home.component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,8 +10,9 @@ export class HomeComponent implements OnInit {
   cardData = [
     {
       header: 'Vingo',
-      bg: '../../../assets/vingo-home.png',
-      description: 'This is a project that I did, it was really fun and really cool. You should ask me about it our hire me please, thank you.',
+      bg: '../../../assets/vingo-worth.png',
+      bg2: '../../../assets/vingo-home.png',
+      description: 'For this client I was responsible for building the front-end of their consumer phone app based off of designs from their designer. After I finished their phone app I was able to go on and build out their consumer and dealership facing websites. I worked closely with all members of the Vingo team to make sure the product I delivered to them was exactly what they asked for.',
       buttons: [
         {
           toolTip: 'Live Site',
@@ -27,49 +28,52 @@ export class HomeComponent implements OnInit {
     },
     {
       header: 'Flight',
+      bg: '../../../assets/flight.png',
       description: 'This is a project that I did, it was really fun and really cool. You should ask me about it our hire me please, thank you.',
       buttons: [
         {
           toolTip: 'Live Site',
           class: 'fas fa-external-link-alt',
-          href: 'vingo.car'
+          href: 'https://flight.run/'
         },
         {
-          toolTip: 'Sorry, this source code is private!',
+          toolTip: 'Source Code',
           class: 'fab fa-github',
-          href: ''
+          href: 'https://github.com/Methodician/flight.run'
         }
       ]
     },
     {
       header: 'ScatterSchool',
+      bg: '../../../assets/scatterschool.png',
       description: 'This is a project that I did, it was really fun and really cool. You should ask me about it our hire me please, thank you.',
       buttons: [
         {
           toolTip: 'Live Site',
           class: 'fas fa-external-link-alt',
-          href: 'vingo.car'
+          href: 'https://scatterschool.com/'
         },
         {
-          toolTip: 'Sorry, this source code is private!',
+          toolTip: 'Source code',
           class: 'fab fa-github',
-          href: ''
+          href: 'https://github.com/Methodician/ScatterSchool'
         }
       ]
     },
     {
       header: 'BIM Earth',
+      bg: '../../../assets/bimearth.png',
       description: 'This is a project that I did, it was really fun and really cool. You should ask me about it our hire me please, thank you.',
       buttons: [
         {
           toolTip: 'Live Site',
           class: 'fas fa-external-link-alt',
-          href: 'vingo.car'
+          href: 'https://bimearth.com/'
         },
         {
-          toolTip: 'Sorry, this source code is private!',
+          toolTip: 'Source Code',
           class: 'fab fa-github',
-          href: ''
+          href: 'https://github.com/Methodician/BIM-Earth'
         }
       ]
     }
